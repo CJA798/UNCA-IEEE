bdd00c9d3f3bb1cb210d49f605ebbdd64a7af4b4
dluebber <89529898+dluebber@users.noreply.github.com>
2022-11-03 12:07:34 -0400

    Add files via upload
    Got the mapping class up, but has errors

---
+++ Map.h
@@ -0,0 +1,11 @@
+void setMovedBot();
+void setColumn();
+
+void setRecycling();
+void setPond();
+int* getColumn();
+int* getPond();
+int* getRecycling();
+
+#define FILLED (500)
+#define MAX_ROTATION (360)
