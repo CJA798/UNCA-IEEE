c58962bf70115cb674367b102095a832451a0c99
ConderChase <conderchase@gmail.com>
2023-03-11 12:50:03 -0500

    VS code new upload

---
+++ Locomotion V2/IEEE Bot Nav System/include/globals.h
@@ -0,0 +1,7 @@
+#include <macros.h>
+
+//Stepper motor_1(X_STEP, X_DIR); // Stepper objects
+//Stepper motor_2(Y_STEP, Y_DIR);
+//Stepper motor_3(Z_STEP, Z_DIR);
+//Stepper motor_4(E0_STEP, E0_DIR);
+//StepControl controller; // Stepper controller object
