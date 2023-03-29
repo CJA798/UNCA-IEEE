7d2c418e0ed685f70d4e9b20f8e4b0a994e82de4
raspi <cjacjacja@hotmail.com>
2023-03-29 14:14:18 -0400

    intgrated PCA

--- Collection/new_ducks/monitor_script.sh
+++ Collection/new_ducks/monitor_script.sh
@@ -7,15 +7,18 @@ SERIAL_PORT="/dev/ttyACM0"
 
 while true; do
     # Run the Python script
-    python communication_test.py
+    python edge_communication_test.py
 
     # Check the exit status of the Python script
     if [ $? -eq 0 ]; then
         echo "communication_test.py exited successfully"
+        echo "RESET" > $SERIAL_PORT
+        sleep 2
     else
         echo "ERROR: communication_test.py exited with an error. Restarting..."
-	# Send a reset signal to the Teensy
-	echo "RESET" > $SERIAL_PORT
+	    # Send a reset signal to the Teensy
+        sleep 2
+	    echo "RESET" > $SERIAL_PORT
     fi
 
     # Wait for a short period before restarting the Python script
