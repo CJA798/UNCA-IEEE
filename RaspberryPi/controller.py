4c5b8a54bbc8b9b4979aa5efff8f778c6abed5a9
raspi <cjacjacja@hotmail.com>
2023-04-05 17:02:29 -0400

    added comments for what needs to be done

--- RaspberryPi/controller.py
+++ RaspberryPi/controller.py
@@ -7,19 +7,25 @@ GPIO.setmode(GPIO.BCM)
 gpio_pins = [pin for pin in range(1,27)]
 gpio_labels = []
 for pin in gpio_pins:
-    GPIO.setup(pin, GPIO.OUT) # set a port/pin as an output   
-    GPIO.output(pin, 0)       # set port/pin value to 1/GPIO.HIGH/True 
+    GPIO.setup(pin, GPIO.OUT) # set a port/pin as an output
+    print("Pin {} Set to OUTPUT".format(pin))   
+    GPIO.output(pin, 0)       # set port/pin value to 1/GPIO.HIGH/True
+    print("Pin {} Set to LOW".format(pin))
 
 camera = CameraSystem()
 
 with camera.camera as cam:
     while True:
-        elevator_data, mid_data, flipper_data = camera.get_data()
+        #elevator_data, mid_data, flipper_data = camera.get_data()
         #data_format: [(class_index, BoundingBox([x,y,w,h]), angle), ...]
-        GPIO.output(7, 1)
-        sleep(1)
-        GPIO.output(7, 0)
-        sleep(1)
+        GPIO.output(21, 1)
+        GPIO.output(20, 1)
+        print("ON")
+        sleep(4)
+        GPIO.output(21, 0)
+        GPIO.output(20, 0)
+        print("OFF")
+        sleep(4)
 
         ''' TODO:
             1)  Trigger GPIO pins according to data gathered
@@ -27,9 +33,21 @@ with camera.camera as cam:
                         -   Put system in safe state: Stop rotating platforms, bring elevator platform down,
                             bring flipper platform to normal position, stop rotating storage drum, keep bracing status
                         -   Activate cleaning mechanism
-                b)  Flipper platform:
-                        - if the angle of the object in the flipper platform is not the target angle, set pin 1 to HIGH
-                        - if the angle of the object is the target angle (or within the threshold), set pin 1 to LOW and pin 2 to HIGH
+                b)  Intake Mechanism:
+                        -   Send through serial line 1
+                c)  Flipper platform:
+                        -   if the angle of the object in the flipper platform is not the target angle, set FLIPPER_ROTATING_PLATFORM to HIGH
+                        -   if the angle of the object is the target angle (or within the threshold):
+                                -   if the object is a duck and is already in storage position, set BRUSH to HIGH 
+                                -   else, set FLIPPER_ROTATING_PLATFORM to LOW and FLIPPER to HIGH* 
+                d)  Elevator Platform, Storage Drum, and Pushers:
+                        -   if the angle of the object in the elevator platform is not the target angle, set ELEVATOR_ROTATING_PLATFORM to HIGH
+                        -   if the angle of the object is the target angle (or within the threshold):
+                                -   if the height is not the target height, set ELEVATOR to HIGH
+                                -   else set ELEVATOR to LOW:
+
+
+                
         '''
 
 
