de4c23b44146986e9230db6b33a59e3677c0a7d6
raspi <cjacjacja@hotmail.com>
2023-04-08 01:37:25 -0400

    final folder added

---
+++ Collection/collection_system/testServos.py
@@ -0,0 +1,18 @@
+import pi_servo_hat
+import time
+
+test = pi_servo_hat.PiServoHat()
+test.restart()
+
+# Moves servo position to 0 degrees (1ms), Channel 0
+test.move_servo_position(0, 0)
+
+while True:
+    for i in range(0, 90):
+        print(i)
+        test.move_servo_position(0, i)
+        time.sleep(.001)
+    for i in range(90, 0, -1):
+        print(i)
+        test.move_servo_position(0, i)
+        time.sleep(.001)
