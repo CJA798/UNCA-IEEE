c4dab4adf2ecbedaeabb55fc3dffd50ca6fccbc5
CJA798 <cjacjacja@hotmail.com>
2023-04-09 02:46:43 -0400

    non edgetpu classifier added & figured out dual model inference

--- Collection/final/camera_test.py
+++ Collection/final/camera_test.py
@@ -7,7 +7,7 @@ try:
         
         while True:
             elevator_data, mid_data, flipper_data = camera.get_data()
-
+            camera.is_duck_standing3()
             if cv2.waitKey(1) == 27:
                 break
 
