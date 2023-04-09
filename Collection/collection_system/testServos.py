e6fd64287738251497419525e68396976a2d2a84
CJA798 <cjacjacja@hotmail.com>
2023-04-09 00:16:19 -0400

    Ivan testServos

--- Collection/collection_system/testServos.py
+++ Collection/collection_system/testServos.py
@@ -5,8 +5,8 @@ test = pi_servo_hat.PiServoHat()
 test.restart()
 
 # Moves servo position to 0 degrees (1ms), Channel 0
-test.move_servo_position(0, 0)
-
+test.move_servo_position(13, 0)
+"""
 while True:
     for i in range(0, 90):
         print(i)
@@ -16,3 +16,6 @@ while True:
         print(i)
         test.move_servo_position(0, i)
         time.sleep(.001)
+"""
+
+
--- Collection/final/testServos.py
+++ Collection/final/testServos.py
@@ -10,30 +10,105 @@ test.restart()
 
 # flips it once
 def flipIt():
-    for i in range(2, -20, -1):
-        test.move_servo_position(0, i)
-        time.sleep(.1)
-    for i in range(-20, 2, 1):
-        test.move_servo_position(0, i)
-        time.sleep(.1)
+    for i in range(0, -20, -1):
+        test.move_servo_position(13, i)
+        time.sleep(.01)
+    for i in range(-20, 0, 1):
+        test.move_servo_position(13, i)
+        time.sleep(.01)
+
+def elevatorUp():
+    test.move_servo_position(1, -48) # Push into Cylinder pos
+    time.sleep(1)
+def elevatorDown():
+    test.move_servo_position(1, 115) # return home elevader pos
+    time.sleep(1)
 
 def kickOff():
-    test.move_servo_position(1, 0) # fast kicking
+    test.move_servo_position(0, 0) # fast kicking
     time.sleep(5)
-    test.move_servo_position(1, 50)
+    test.move_servo_position(0, 50)
 
+# let me know when you are using this function, requires tweaking 
 def pushOnElevator():
-    test.move_servo_position(1, 80) # fast-ish pushing
+    test.move_servo_position(0, 80) # fast-ish pushing
     time.sleep(1.5)
-    test.move_servo_position(1, 50) # stops
+    test.move_servo_position(0, 50) # stops
     time.sleep(1)
-    test.move_servo_position(1, 0)
+    test.move_servo_position(0, 0)
     time.sleep(.77)
-    test.move_servo_position(1, 50)
+    test.move_servo_position(0, 50)
     time.sleep(1)
 
-# test.move_servo_position(2, -48) # Push into Cylinder pos
+def backReady():
+    for i in range(120, 0, -1):
+        test.move_servo_position(8, i)
+        time.sleep(.005)
+    for i in range(52, 2, -1):
+        test.move_servo_position(7, i)
+        time.sleep(.005)
+    for i in range(135, 10, -1):
+        test.move_servo_position(9, i)
+        time.sleep(.005)
 
-# test.move_servo_position(2, 112) # return home elevader pos
+def backInit():
+    for i in range(10, 135, 1):
+        test.move_servo_position(9, i)
+        time.sleep(.005)
+    for i in range(0, 53, 1):
+        test.move_servo_position(7, i)
+        time.sleep(.005)
+    for i in range(0, 118, 1):
+        test.move_servo_position(8, i)
+        time.sleep(.005)
 
-test.move_servo_position(3, 90)
+def shakeBack():
+    test.move_servo_position(9, 10)
+    test.move_servo_position(8, 5)
+    time.sleep(0.5)
+    test.move_servo_position(8, 0)
+    test.move_servo_position(9, 5)
+    time.sleep(0.5)
+
+def twoTowerSet():
+    for i in range(10, 135, 1):
+        test.move_servo_position(9, i)
+        time.sleep(.005)
+    for i in range(0, 53, 1):
+        test.move_servo_position(7, i)
+        time.sleep(.005)
+    for i in range(0, 118, 1):
+        test.move_servo_position(8, i)
+        time.sleep(.005)
+    for i in range(135, 10, -1):
+        test.move_servo_position(9, i)
+        time.sleep(.005)
+
+# has to be in twoTowerSet to work
+def twoTowerDrop():
+    for i in range(10, 135, 1):
+        test.move_servo_position(9, i)
+        time.sleep(.005)
+
+def twoTowerClose():
+    for i in range(135, 10, -1):
+        test.move_servo_position(9, i)
+        time.sleep(.005)
+
+def threeTowerDrop():
+    for i in range(10, 135, 1):
+        test.move_servo_position(9, i)
+        time.sleep(.005)
+    for i in range(0, 118, 1):
+        test.move_servo_position(8, i)
+        time.sleep(.005)
+
+def threeTowerClose():
+    for i in range(120, 0, -1):
+        test.move_servo_position(8, i)
+        time.sleep(.005)
+    for i in range(135, 10, -1):
+        test.move_servo_position(9, i)
+        time.sleep(.005)
+
+
