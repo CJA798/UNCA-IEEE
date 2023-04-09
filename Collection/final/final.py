f3cc4e6964f9e0b67dec762a2b1e10b7a8f60670
dluebber <dluebber@unca.edu>
2023-04-08 20:20:17 -0400

    I have the pink Duck case done. Need other towers

--- Collection/final/final.py
+++ Collection/final/final.py
@@ -58,7 +58,7 @@ def main():
                 if serial_stepper.in_waiting > 0:
                     DrumStatus = serial.readline().decode('utf-8').rstrip()
                     
-                if DrumStatus == "Output":
+                if DrumStatus == "Output" and len(elevator_data) < 1 and len(flipper_data) < 1:
                     break
                 
 
@@ -67,7 +67,7 @@ def main():
     finally:
         cv2.destroyAllWindows()
         
-        
+    DrumStatus = 0
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     #This is the outpt function, It should be hard coded depending on the serial from the navigation Teensy
     while True:
@@ -76,14 +76,98 @@ def main():
             currentTower = serial.readline().decode('utf-8').rstrip()
         
         if currentTower != '':
-            if currentTower == TOWER_ONE:
-                sequenceForTower1()
-            if currentTower == TOWER_TWO:
-                sequenceForTower2()
-            if currentTower == DUCK_TOWER:
-                sequenceForTower3()
+            WaitingToFinish = 0
+            
+            while not WaitingToFinish:
+                if currentTower == TOWER_ONE:
+                    WaitingToFinish = sequenceForTower1(serial_stepper, robot)
+                if currentTower == TOWER_TWO:
+                    WaitingToFinish = sequenceForTower2(serial_stepper, robot)
+                if currentTower == DUCK_TOWER:
+                    WaitingToFinish = sequenceForTower3(serial_stepper, robot)
         
 
+
+def sequenceForTower1(serial_stepper, robot: Robot):
+    pass
+
+def sequenceForTower2(serial_stepper, robot: Robot):
+    pass
+
+def sequenceForTower3(serial_stepper, robot: Robot):
+    #This will be the first case. Has to be, it will mean we can just make the tower
+    #Cases to be considered. Do we have all the objects for this tower.
+    PusherPosition = 0
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #Case where the red/green is stored perfectly and I only need to push from that location for columns
+    if columnPosition[0] == 3:
+        #perfect case.
+        if len(columnPosition) > 0:
+            PusherPosition = outputSerial(serial_stepper, 'E')
+        if PusherPosition == PusherStatus.READY and len(columnPosition) > 0:
+            robot.CollectionSystem.Pushers.UnloadingPillarPusherTop
+            sleep(.50)
+            robot.CollectionSystem.Pushers.RetractPusherTop
+            PusherPosition = PusherStatus.RETRACTED
+            columnPosition.clear
+        
+        if len(columnPosition) == 0:
+            PusherPosition = outputSerial(serial_stepper, 'B')
+        if PusherPosition == PusherStatus.READY and len(columnPosition) == 0:
+            #Once this if statement finishes then we tell waiting to finish that we are done and need to wait for the next tower to be ready to be built
+            robot.CollectionSystem.Pushers.UnloadingPillarPusherTop
+            sleep(.50)
+            robot.CollectionSystem.Pushers.RetractPusherTop
+            PusherPosition = PusherStatus.RETRACTED
+            return 1
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #The red/green columns are backwards so I need to unload a green column from the other slot then the red. Finally the pink
+    elif columnPosition[0] == 4:
+        if greenColumnCounter > 2:
+            PusherPosition = outputSerial(serial_stepper, 'D')
+        if PusherPosition == PusherStatus.READY and greenColumnCounter > 2:
+            robot.CollectionSystem.Pushers.Half_UnloadingPillarPusherTop
+            sleep(.50)
+            robot.CollectionSystem.Pushers.RetractPusherTop
+            PusherPosition = PusherStatus.RETRACTED
+            greenColumnCounter = greenColumnCounter - 1
+        
+        if greenColumnCounter == 2 and len(columnPosition) > 1:
+            PusherPosition = outputSerial(serial_stepper, 'E')
+        if PusherPosition == PusherStatus.READY and greenColumnCounter == 2 and len(columnPosition) > 1:
+            robot.CollectionSystem.Pushers.Half_UnloadingPillarPusherTop
+            sleep(.50)
+            robot.CollectionSystem.Pushers.RetractPusherTop
+            PusherPosition = PusherStatus.RETRACTED
+            columnPosition.pop
+        
+        if greenColumnCounter == 2 and columnPosition == 1:
+            PusherPosition = outputSerial(serial_stepper, 'B')
+        if PusherPosition == PusherStatus.READY and greenColumnCounter == 2 and len(columnPosition) > 1:
+            #Once this if statement finishes then we tell waiting to finish that we are done and need to wait for the next tower to be ready to be built
+            robot.CollectionSystem.Pushers.UnloadingPillarPusherTop
+            sleep(.50)
+            robot.CollectionSystem.Pushers.RetractPusherTop
+            PusherPosition = PusherStatus.RETRACTED
+            return 1
+    #If the tower isn't build yet then tell the while loop to continue
+    return 0
+            
+        
+    pass
+
+def outputSerial(serial_stepper, position):
+    if DrumStatus == 0:
+        serial.reset_output_buffer()
+        str_data = position
+        serial.write(str_data.encode())
+        DrumStatus = 1
+    elif DrumStatus == 1:
+        if serial_stepper.in_waiting > 0:
+            DrumStatus = serial.readline().decode('utf-8').rstrip()
+    elif DrumStatus == 'R':
+        DrumStatus = 0
+        return PusherStatus.READY
 
 if __name__ == '__main__':
   main()
@@ -233,9 +317,9 @@ def drumSerial(item, serial_stepper):
         position = 'e'
     
     if DrumStatus == 0:
-        serial.reset_output_buffer()
+        serial_stepper.reset_output_buffer()
         str_data = position
-        serial.write(str_data.encode())
+        serial_stepper.write(str_data.encode())
         DrumStatus = 1
     elif DrumStatus == 1:
         if serial_stepper.in_waiting > 0:
