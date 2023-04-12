1f77ce12ff902c23642e1bca5be21bcb0591eb78
meturcio <meturcio@ncsu.edu>
2023-04-12 19:41:15 -0400

    Create TestingLED.py

---
+++ Collection/collection_system/TestingLED.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+# set up GPIO pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2, GPIO.IN) # replace 17 with the GPIO pin number you are using
+GPIO.setup(3, GPIO.OUT) # replace 18 with the GPIO pin number for the LED
+
+# wait for limit switch to be activated
+while not GPIO.input(2):
+    time.sleep(0.1)
+
+# turn on LED when limit switch is activated
+GPIO.output(3, GPIO.HIGH)
+
+# print message when limit switch is activated
+print("Limit switch activated")
+
+# wait for a few seconds
+time.sleep(5)
+
+# turn off LED
+GPIO.output(3, GPIO.LOW)
+
+# clean up GPIO pins
+GPIO.cleanup()
