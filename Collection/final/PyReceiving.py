4ce4de88776c8b8f24e517c453600cdec88789b7
CJA798 <cjacjacja@hotmail.com>
2023-04-13 03:41:47 -0400

    4am before competition

--- Collection/final/PyReceiving.py
+++ Collection/final/PyReceiving.py
@@ -1,34 +1,18 @@
 import time
 import serial
-      
+    
 
 
+# define button pin
+# check if pin is HIGH/Low
+# if button is pressed :
 
 
-ser = serial.Serial(            
-port='/dev/ttyACM0', #decide which port
-baudrate = 9600,
-
-'''
-parity=serial.PARITY_NONE,
-stopbits=serial.STOPBITS_ONE,
-bytesize=serial.EIGHTBITS,
-'''
-
- timeout=1
-)
+ser = serial.Serial(port='/dev/ttyACM0',baudrate = 9600,timeout=1)
 
 ser.reset_input_buffer()
 
 while True:
-    #n = random.randint(1,9)
-    
     if(ser.in_waiting > 0):
      message = ser.readline().decode('utf-8').rstrip()
-     #ser.read("Start up")
      print(message)
-     
-     #time.sleep(1)
-
-     #feedback = ser.read()
-     #print(feedback) #Expecting 'X'
