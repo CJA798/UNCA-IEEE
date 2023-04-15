f95411f2214a6e2eac77496ef57b770493084d02
erholling <erhollin@ncsu.edu>
2023-04-15 03:12:59 -0400

    Update data_backup.py
    Writing functions for updating backup file & tower state stuff

--- Collection/final/data_backup.py
+++ Collection/final/data_backup.py
@@ -65,6 +65,63 @@ print(c)
 print(d)
 print(e)
 
+#stuff above if reading what's in the file, will read the current data when pi dies and feed it into the counter values
+
+def updatingData(filename, yellowDucks, pinkDucks, whiteColumns, greenColumns, redColPos: []):
+'''    echo "" > filename      #clearing data
+
+    # writing new data to file
+    with open(filename, 'w') as f:
+        for i in range(0, yellowDucks):     #0 for yellow ducks
+            f.write(f'0\n')
+        for i in range(0, pinkDucks):       #1 for pink duck
+            f.write(f'1\n')
+        for i in range(0, whiteColumns):    #2 for white columns
+            f.write(f'2\n')
+        for i in range(0, greenColumns):    #3 for green columns
+            f.write(f'3\n')
+        if len(redColPos) > 0 and redColPos[0] == 4
+            f.write(f'4, 1\n')          #write any 
+        elif len(redColPos) > 1 and redColPos[1] == 4
+            f.write(f'4, 2\n')
+    '''
+    
+
+def shortTowerOne(bigState, towerState, filename, yellowDucks, pinkDucks, whiteColumns, greenColumns, redColPos):
+    #store what we now have in drum & states. this gets called whenever something gets pushed out, and we're building the first small tower
+    '''echo "" > filename
+    
+    with open(filename, 'w') as f:
+        f.write('2\n')      #writing that we're in big state 2 (collection state)
+        f.write('0\n')      #writing that we're in tower state 0 (first small tower)
+
+    updatingData(filename, yellowDucks, pinkDucks, whiteColumns, greenColumns, redColPos)
+    '''
+        
+
+def shortTowerTwo(bigState, towerState, filename, yellowDucks, pinkDucks, whiteColumns, greenColumns, redColPos):
+    #store what we now have in drum & states. this gets called whenever something gets pushed out, and we're building the second small tower
+    '''echo "" > filename
+
+    with open(filename, 'w') as f:
+        f.write('2\n')      #writing that we're in big state 2 (collection state)
+        f.write('1\n')      #writing that we're in tower state 1 (second small tower)
+
+    updatingData(filename, yellowDucks, pinkDucks, whiteColumns, greenColumns, redColPos)
+    '''
+
+
+def bigTower(bigState, towerState, filename, yellowDucks, pinkDucks, whiteColumns, greenColumns, redColPos):
+    #store what we now have in drum & states. this gets called whenever something gets pushed out, and we're building the big tower
+    '''echo "" > filename
+
+    with open(filename, 'w') as f:
+        f.write('2\n')      #writing that we're in big state 2 (collection state)
+        f.write('2\n)       #writing that we're in tower state 2 (big tower)
+
+    updatingData(filename, yellowDucks, pinkDucks, whiteColumns, greenColumns, redColPos)
+    '''    
+
 
 def StateStartBackup():
     
